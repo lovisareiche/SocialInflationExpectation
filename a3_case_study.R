d9c9703896cc04be53710a67133c1c03fbe20ebe
Lovisa Reiche <lovisa.reiche@gmx.de>
2022-08-16 09:48:40 +0200

    case study using Nakamura data

--- a3_case_study.R
+++ a3_case_study.R
@@ -33,7 +33,7 @@ sci_control <- read_tsv("../SocialInflationExpectation/_intermediate/sci_cz_cz_c
 
 # CPI
 # Built in b1_create_state_cpi
-cpi <- read_csv("../SocialInflationExpectation/_intermediate/cpi_cz2000-timeseries.csv")
+cpi <- read_csv("../SocialInflationExpectation/_intermediate/cpi_cz2000-timeseries_nakamura.csv")
 
 # Inflex
 # Built in a1_county_data_collect
@@ -45,20 +45,27 @@ inflex <- read_csv("../SocialInflationExpectation/_intermediate/inflexp_date_cz.
 ################################
 
 # cpi with dates we have expectations for
-cpi_used <- cpi[cpi$Date <= max(inflex$date),]
+cpi_used <- cpi[cpi$date <= max(inflex$date) & cpi$date >= min(inflex$date),]
 
 
 # 2.1 Date-CZ pairs with highest cpi in levels
 ##############################################
 
-cpi_max1 <- cpi_used[cpi_used$cpi_mean == max(cpi_used$cpi_mean),]
+#cpi_max1 <- cpi_used[cpi_used$cpi_mean == max(cpi_used$cpi_mean),]
 
 
 # 2.2 Date-CZ pairs with the highest cpi month-on-month change
 ##############################################################
 
-cpi_max2 <- cpi_used[cpi_used$cpi_mom_mean == max(cpi_used$cpi_mom_mean),]
+#cpi_max2 <- cpi_used[cpi_used$cpi_mom_mean == max(cpi_used$cpi_mom_mean),]
 
+# 2.3 Date-CZ pairs with the highest average 12 months inflation rate 
+##############################################################
+
+cpi_max3 <- cpi_used[cpi_used$pi_mean == max(cpi_used$pi_mean),]
+
+# what was the average inflation in this time?
+mean(cpi_used$pi_mean[cpi_used$date == min(cpi_max3$date)])
 
 #############################################################
 ##### 3. Match inflation expectations to sci with cases #####
@@ -80,6 +87,11 @@ inflex_sub2 <- inflex %>%
   group_by(cz2000) %>%
   mutate(chg_inflex_med = inflexp_median - dplyr::lag(inflexp_median)) %>%
   filter(date == unique(cpi_max2$Date))
+
+inflex_sub3 <- inflex %>%
+  group_by(cz2000) %>%
+  mutate(chg_inflex_med = inflexp_median - dplyr::lag(inflexp_median)) %>%
+  filter(date == min(cpi_max3$date))
 
 # Note that this is less than for all commuting zones because for some we do not have data for these specific months
 
@@ -108,6 +120,15 @@ dat2 <- sci_control %>%
   ungroup %>%
   unique
 
+dat3 <- sci_control %>%
+  # inflation expectations in the user's location
+  inner_join(inflex_sub3, by = c("user_loc" = "cz2000")) %>%
+  # share of connections with cpi experience
+  group_by(user_loc) %>%
+  summarise(share_high = sum(share_sci[fr_loc %in% cpi_max3$cz2000]), chg_inflex_med, inflexp_median) %>%
+  ungroup %>%
+  unique
+
 # save scatter plot for case 1
 png("../SocialInflationExpectation/_output/case_study_1.png", width = 500, height = 500)
 plot(dat1$share_high,dat1$chg_inflex_med, main = "Case Study 1", xlab = "Share of friends in high inflation region", ylab="Median inflation expectations")
@@ -120,6 +141,11 @@ plot(dat2$share_high,dat2$chg_inflex_med, main = "Case Study 2", xlab = "Share o
 text(paste("Correlation:", round(cor(dat2$share_high,dat2$chg_inflex_med), 2)), x = 0.2, y = 60)
 dev.off()
 
+# save scatter plot for case 3
+png("../SocialInflationExpectation/_output/case_study_3.png", width = 500, height = 500)
+plot(dat3$share_high,dat3$inflexp_median, main = "Case Study 3", xlab = "Share of friends in high inflation region", ylab="Median inflation expectations")
+dev.off()
+
 
 #################################################
 ##### 4. Show expectations before and after #####
@@ -128,45 +154,34 @@ dev.off()
 # function to add and subtract months
 add.months= function(date,n) seq(date, by = paste (n, "months"), length = 2)
 
+# set which case
+cpi_max <- cpi_max3
 
-inflex_sub3 <- inflex %>%
+
+inflex_sub<- inflex %>%
   # only use period identified in case plus one before and one after
-  filter(date == unique(cpi_max1$Date) | date == add.months(unique(cpi_max1$Date),1)[2] | date == add.months(unique(cpi_max1$Date),-1)[2])
+  filter(date == min(cpi_max$date) | date == add.months(max(cpi_max$date),1)[2] | date == add.months(min(cpi_max$date),-1)[2])
 
 
-dat3 <- sci_control %>%
+dat <- sci_control %>%
   arrange(user_loc,fr_loc) %>%
   # inflation expectations in the user's location
-  inner_join(inflex_sub3, by = c("user_loc" = "cz2000")) %>%
+  inner_join(inflex_sub, by = c("user_loc" = "cz2000")) %>%
   # share of connections with cpi experience
   group_by(user_loc) %>%
-  summarise(share_high = sum(share_sci[fr_loc %in% cpi_max1$cz2000]), inflexp_median, date) %>%
+  summarise(share_high = sum(share_sci[fr_loc %in% cpi_max$cz2000]), inflexp_median, date) %>%
   ungroup %>%
   unique
 
 
-inflex_sub4 <- inflex %>%
-  # only use period identified in case plus one before and one after
-  filter(date == unique(cpi_max2$Date) | date == add.months(unique(cpi_max2$Date),1)[2] | date == add.months(unique(cpi_max2$Date),-1)[2])
-
-
-dat4 <- sci_control %>%
-  arrange(user_loc,fr_loc) %>%
-  # inflation expectations in the user's location
-  inner_join(inflex_sub4, by = c("user_loc" = "cz2000")) %>%
-  # share of connections with cpi experience
-  group_by(user_loc) %>%
-  summarise(share_high = sum(share_sci[fr_loc %in% cpi_max2$cz2000]), inflexp_median, date) %>%
-  ungroup %>%
-  unique
 
 
 # save scatter plot for case 4
 png("../SocialInflationExpectation/_output/case_study_4.png", width = 500, height = 500)
 # plot points prior to inflation exposure in red
-plot(dat4$share_high[dat4$date == add.months(unique(cpi_max2$Date),-1)[2]],dat4$inflexp_median[dat4$date == add.months(unique(cpi_max2$Date),-1)[2]], main = "Case Study 4", xlab = "Share of friends in high inflation region", ylab="Median inflation expectations", col = 2)
+plot(dat$share_high[dat$date == add.months(min(cpi_max$date),-1)[2]],dat$inflexp_median[dat$date == add.months(min(cpi_max$date),-1)[2]], main = "Case Study 4", xlab = "Share of friends in high inflation region", ylab="Median inflation expectations", col = 2)
 # plot points during inflation exposure in green
-points(dat4$share_high[dat4$date == add.months(unique(cpi_max2$Date),1)[1]],dat4$inflexp_median[dat4$date == add.months(unique(cpi_max2$Date),1)[1]], col = 3)
+points(dat$share_high[dat$date == min(cpi_max$date)],dat$inflexp_median[dat$date == min(cpi_max$date)], col = 3)
 # plot points after inflation exposure in blue
-points(dat4$share_high[dat4$date == add.months(unique(cpi_max2$Date),1)[2]],dat4$inflexp_median[dat4$date == add.months(unique(cpi_max2$Date),1)[2]], col = 4)
+points(dat$share_high[dat$date == add.months(max(cpi_max$date),1)[2]],dat$inflexp_median[dat$date == add.months(max(cpi_max$date),1)[2]], col = 4)
 dev.off()
