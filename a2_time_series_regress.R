f962e330fdd341986f7ba33041c869ed4dee520e
Lovisa Reiche <lovisa.reiche@gmx.de>
2022-08-21 12:42:59 +0200

    using difference measure instead

--- a2_time_series_regress.R
+++ a2_time_series_regress.R
@@ -38,16 +38,23 @@ dat_inflex <- read_csv("../SocialInflationExpectation/_intermediate/inflexp_date
 sci_weighted_inflation_control <- read_csv("../SocialInflationExpectation/_intermediate/sci_weighted_inflation_control.csv") %>%
   rename(sci_weighted_inflation_control=sci_weighted_inflation)
 sci_weighted_inflation <- read_csv("../SocialInflationExpectation/_intermediate/sci_weighted_inflation.csv")
+SPI <- read_csv("../SocialInflationExpectation/_intermediate/SPI.csv")
 
 # Read in distance-weighted inflation (Physical Proximity to Inflation)
 # Built in a1_county_data_collect.R
 dist_weighted_inflation <- read_csv("../SocialInflationExpectation/_intermediate/dist_weighted_inflation.csv")
-
+PPI <- read_csv("../SocialInflationExpectation/_intermediate/PPI.csv")
 
 # Join the weighted cases measures
 weighted_inflation_dat <- dist_weighted_inflation %>% 
   left_join(sci_weighted_inflation_control, by=c("cz1"="user_loc", "date"="date")) %>%
   left_join(sci_weighted_inflation, by=c("cz1"="user_loc", "date"="date")) %>%
+  rename(cz2000 = cz1)
+
+
+# Join the weighted cases measures
+dat <- PPI %>% 
+  left_join(SPI, by=c("cz1"="user_loc", "date"="date")) %>%
   rename(cz2000 = cz1)
 
 
@@ -80,13 +87,13 @@ dat_cpi <- read_csv("../SocialInflationExpectation/_intermediate/cpi_cz2000-time
 
 regress_dat <- dat_inflex %>% 
   # Join with social and physical distance to experiences inflation
-  inner_join(weighted_inflation_dat) %>% 
+  inner_join(dat) %>% 
   # Join with covariates (!!! No time variation here !!!)
   left_join(dat_covariates) %>% 
   # Join with cpi (!!! Only availabe in 2018, on regional not cz basis !!!)
-  inner_join(dat_cpi) %>%
+  inner_join(dat_cpi) 
   # name as in paper
-  rename(SPI = sci_weighted_inflation, SPI_control = sci_weighted_inflation_control, PPI = dist_weighted_inflation) #%>%
+  #rename(SPI = sci_weighted_inflation, SPI_control = sci_weighted_inflation_control, PPI = dist_weighted_inflation) #%>%
   # Make the lagged versions of our variables
   #group_by(cz2000) %>% 
   #arrange(cz2000, date) %>% 
@@ -108,20 +115,12 @@ write_csv(regress_dat, "../SocialInflationExpectation/_output/time_series_regres
 ##### 3. Panel regression #####
 ###############################
 
-
 # estimate the fixed effects regression, no control with plm()
 inflex_fe_mod <- plm(inflexp_median ~ SPI + PPI + pi_mean, 
                     data = regress_dat,
                     index = c("cz2000", "date"), 
                     model = "within")
 coeftest(inflex_fe_mod)
-
-# estimate the fixed effects regression, control with plm()
-inflex_fe_mod_c <- plm(inflexp_median ~ SPI_control + PPI + pi_mean, 
-                     data = regress_dat,
-                     index = c("cz2000", "date"), 
-                     model = "within")
-coeftest(inflex_fe_mod_c)
 
 # estimate the pooled effects regression, no control with plm()
 inflex_po_mod <- plm(inflexp_median ~ SPI + PPI + pi_mean + poor_share2010 + med_hhinc2016 + rent_twobed2015, 
@@ -131,12 +130,14 @@ inflex_po_mod <- plm(inflexp_median ~ SPI + PPI + pi_mean + poor_share2010 + med
 coeftest(inflex_po_mod)
 
 
-# estimate the pooled effects regression, control with plm()
-inflex_po_mod_c <- plm(inflexp_median ~ SPI_control + PPI + pi_mean + poor_share2010 + med_hhinc2016 + rent_twobed2015, 
+
+# estimate the pooled effects regression, no control with plm()
+inflex_po_mod_test1 <- plm(inflexp_median ~ SPI + pi_mean + poor_share2010 + med_hhinc2016 + rent_twobed2015, 
                      data = regress_dat,
                      index = c("cz2000", "date"), 
                      model = "pooling")
-coeftest(inflex_po_mod_c)
+coeftest(inflex_po_mod_test1)
+
 
 
 # Points to Note:
@@ -145,20 +146,12 @@ coeftest(inflex_po_mod_c)
 # - some cz have only very few people answering the inflation expectations survey so the medians may be driven by very few people
 
 
-png("../SocialInflationExpectation/_intermediate/SCI weighted_no control.png", width = 500, height = 500)
-plot(regress_dat$inflexp_median,regress_dat$sci_weighted_inflation, main = "SCI weighted", xlab = "Median expectation", ylab="SCI weighted expectation")
-text(paste("Correlation:", round(cor(regress_dat$inflexp_median,regress_dat$sci_weighted_inflation), 2)), x = -50, y = 70)
+png("../SocialInflationExpectation/_intermediate/SPI.png", width = 500, height = 500)
+plot(regress_dat$inflexp_median,regress_dat$SPI, main = "SCI weighted", xlab = "Median expectation", ylab="SCI weighted expectation")
 dev.off()
 
-png("../SocialInflationExpectation/_intermediate/SCI weighted.png", width = 500, height = 500)
-plot(regress_dat$inflexp_median,regress_dat$sci_weighted_inflation_control, main = "SCI weighted", xlab = "Median expectation", ylab="SCI weighted expectation")
-text(paste("Correlation:", round(cor(regress_dat$inflexp_median,regress_dat$sci_weighted_inflation_control), 2)), x = -50, y = 70)
-dev.off()
-
-
-png("../SocialInflationExpectation/_intermediate/Dist weighted.png", width = 500, height = 500)
-plot(regress_dat$inflexp_median,regress_dat$dist_weighted_inflation, main = "Dist weighted", xlab = "Median expectation", ylab="SCI weighted expectation")
-text(paste("Correlation:", round(cor(regress_dat$inflexp_median,regress_dat$sci_weighted_inflation), 2)), x = -50, y = 70)
+png("../SocialInflationExpectation/_intermediate/PPI.png", width = 500, height = 500)
+plot(regress_dat$inflexp_median,regress_dat$PPI, main = "Dist weighted", xlab = "Median expectation", ylab="SCI weighted expectation")
 dev.off()
 
 
