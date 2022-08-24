dfb041b8571ead3f1e20414eba98f59d38c743fb
Lovisa Reiche <lovisa.reiche@gmx.de>
2022-08-24 16:11:52 +0200

    population weighted sci measure when aggregating from county to commuting zone

--- a1_county_data_collect.R
+++ a1_county_data_collect.R
@@ -25,6 +25,7 @@ rm(list=ls())
 #     3. Create Proximity to Inflation (social and physical)
 
 
+rm(list = ls())
 library(tidyverse)
 library(lubridate)
 library(pracma)
@@ -59,6 +60,10 @@ dir.inflexp <- "../SocialInflationExpectation/_input/FRBNY-SCE-Public-Microdata-
 # https://www.ers.usda.gov/data-products/commuting-zones-and-labor-market-areas/
 dir.geo <- "../SocialInflationExpectation/_input/cz00_eqv_v1.csv"
 
+# Population
+# https://www.census.gov/data/datasets/time-series/demo/popest/2020s-counties-total.html#par_textimage_70769902
+dir.pop <- "../SocialInflationExpectation/_input/co-est2021-alldata.csv"
+
 
 
 ################################
@@ -72,6 +77,12 @@ dir.geo <- "../SocialInflationExpectation/_input/cz00_eqv_v1.csv"
 dat_geo <- read_csv2(dir.geo) %>%
   # select only fips and 2000 commuting zones
   select(fips = FIPS, cz2000 = "Commuting Zone ID, 2000", cz1990 = "Commuting Zone ID, 1990")
+
+# Read in population size in each county to aggregate population weighted
+dat_pop <- read_csv(dir.pop) %>%
+  # create fips index for counties by combining state and county code
+  unite("fips",STATE:COUNTY, sep = "") %>%
+  select(fips, pop = POPESTIMATE2021)
 
 # Read in Covariates, goal: assign 2000 cz to all areas in covariates
 dat_covariates <- read_csv(dir.covariates) %>%
@@ -90,17 +101,52 @@ dat_sci <- read_tsv(dir.county_county_sci) %>%
   subset(select = c(user_loc,fr_loc,scaled_sci,fr_loc_cz)) %>%
   inner_join(dat_geo,by = c("user_loc"="fips")) %>%
   rename(user_loc_cz = cz2000) %>%
-  subset(select = c(user_loc,fr_loc,scaled_sci,fr_loc_cz,user_loc_cz))
+  subset(select = c(user_loc,fr_loc,scaled_sci,fr_loc_cz,user_loc_cz)) %>%
+  # join with population data
+  inner_join(dat_pop, by = c("user_loc"="fips")) %>%
+  rename(user_loc_pop = pop) %>%
+  inner_join(dat_pop, by = c("fr_loc"="fips")) %>%
+  rename(fr_loc_pop = pop) 
 
 # Collapse by combining all from one location
 for (i in unique(dat_sci$user_loc_cz)) {
   
   print(i)
   
+  # create subset for cz2000 i
   us_subset <- filter(dat_sci,user_loc_cz == i)
-  U = uniq(us_subset$fr_loc_cz)
-  a = accumarray(U$n,us_subset$scaled_sci)
   
+  # split it by user loc counties to make sure we weigh all
+  for (ii in unique(us_subset$user_loc)){
+    us_subset_sub <- us_subset[us_subset$user_loc == ii,] %>%
+      # we want to weigh counties in each foreign commuting zone
+      group_by(fr_loc_cz) %>%
+      # sum population in this commuting zone
+      mutate(total_pop = sum(fr_loc_pop)) %>%
+      ungroup %>%
+      # calculate population share in each foreign county when aggregating sci
+      mutate(weight_fr = fr_loc_pop/total_pop)
+  
+    if (ii == unique(us_subset$user_loc)[1]){
+      S <- us_subset_sub
+    } else {
+      S <- rbind(S,us_subset_sub)
+    }
+    }
+  S <- S %>%
+    # weight of county within user commuting zone
+    mutate(weight_user = user_loc_pop/sum(unique(user_loc_pop))) %>%
+    # weighted sci
+    mutate(weighted_sci = scaled_sci * weight_fr * weight_user)
+
+  
+  # index of each county that occurs within this cz2000
+  U = uniq(S$fr_loc_cz)
+  
+  # acummulate sci
+  a = accumarray(U$n,S$weighted_sci)
+  
+  # collapse all user locs into one
   if (i==dat_sci$user_loc_cz[1]){
     dat_sci_final <- tibble(user_loc = rep(i,times=length(a)), fr_loc = as.character(U$b), sci = a)
   } else {
@@ -108,6 +154,9 @@ for (i in unique(dat_sci$user_loc_cz)) {
   }
   
 }
+
+# clean up
+rm(us_subset, us_subset_sub,a,U,i,ii)
 
 # Get the share of total SCI from each county
 dat_sci_final <- dat_sci_final %>%
